e79b9a1587c5f4cc5be5202ddbeb1a6de3fea5a0
feuster <alexander.feuster@gmail.com>
2024-09-07 18:15:46 +0200

    Refactoring
    Refactoring:
    - Bounce2 library used now for debouncing the pushbutton
    - login credentials now stored in const for easier change
    - code cleanup

--- HID-Button.ino
+++ HID-Button.ino
@@ -1,5 +1,5 @@
 //--------------------------------------------//
-//              HID-Button V1.0               //
+//              HID-Button V1.1               //
 //         (c) Alexander Feuster 2024         //
 //  http://www.github.com/feuster/HID-Button  //
 //--------------------------------------------//
@@ -7,23 +7,40 @@
 // define german keyboard layout
 #define kbd_de_de
 #include "DigiKeyboard.h"
+#include "Bounce2.h"
 
-// variable definitions
+/// --------------------------------------------------------------------------------------------
+/// login credentials which can be changed to real credentials
+/// --------------------------------------------------------------------------------------------
+const char USER[] = "USER";
+const char PASSWORD[] = "PASSWORD";
+// do not change code behind this line if not needed !!!
+/// --------------------------------------------------------------------------------------------
+
+/// --------------------------------------------------------------------------------------------
+/// hardware definitions
+/// --------------------------------------------------------------------------------------------
 // input pin for pushbutton
 const int buttonPin = PB0;
-// for checking the state of a pushButton
-int previousButtonState = HIGH;
 // state of te builtin LED
 int previousLedState = HIGH;
+// create a Bounce2::Button object
+Bounce2::Button button = Bounce2::Button();
 
-
-// start the initialization
+/// --------------------------------------------------------------------------------------------
+/// start the initialization
+/// --------------------------------------------------------------------------------------------
 void setup() {
-  // make the pushButton pin an input:
+  // configure the pushbutton pin as input:
   pinMode(buttonPin, INPUT_PULLUP);
 
-  // wait some time before first run, to give target time to initialize
+  // wait some time before first run, to give hardware time to initialize
   DigiKeyboard.delay(2500);
+
+  // Bounce2::Button configuration
+  button.attach(buttonPin, INPUT_PULLUP);
+  button.interval(100);
+  button.setPressedState(LOW); 
 
   // blink the builtin LED as start confirmation
   for (int i = 0; i < 10; i++)
@@ -35,37 +52,33 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 }
 
+/// program loop
 void loop() {
-  // program loop start
+  // read the pushbutton state
+  button.update();
 
-  // read the pushbutton:
-  int buttonState = digitalRead(buttonPin);
-
-  // if the button state has changed, and it's currently pressed:
-  if ((buttonState != previousButtonState) && (buttonState == HIGH)) 
+  // if the button state has changed, and it's currently pressed type login credentials
+  if (button.pressed()) 
   {
-    // turn the LED on while message is typed
+    // turn the builtin LED on while login is typed
     digitalWrite(LED_BUILTIN, HIGH);
 
-    // type out a message
-    DigiKeyboard.print("USER");
-    DigiKeyboard.delay(75);
+    // type out login credentials (remark: sendKeyStroke(0) is used as workaround for compatibility problems with older hardware and can be removed if this isn't needed)
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.print(USER);
+    DigiKeyboard.delay(125);
+    DigiKeyboard.sendKeyStroke(0);
     DigiKeyboard.sendKeyStroke(43); //translates to TAB
-    DigiKeyboard.delay(75);
-    DigiKeyboard.print("PASSWORD");
-    DigiKeyboard.delay(75);
-    DigiKeyboard.sendKeyStroke(0x28); //translates to ENTER
-    DigiKeyboard.delay(75);
+    DigiKeyboard.delay(125);
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.print(PASSWORD);
+    DigiKeyboard.delay(125);
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER);
 
-    // turn the LED off after message has been typed
+    // turn the builtin LED off after login has been typed
     digitalWrite(LED_BUILTIN, LOW);
   }
 
-  // forced delay for simple debouncing the hardware button
-  delay(100);
-
-  // save the current button state for comparison next time:
-  previousButtonState = buttonState;
-
-  //program loop end and repeat
+  ///program loop ends and will repeat now in a new cycle
 }
--- libraries/readme.txt
+++ libraries/readme.txt
@@ -1,4 +1,6 @@
-Extract zip archive "DigisparkKeyboard-master-fixed.zip" to C:\Users\<YOURUSERNAME>\AppData\Local\Arduino15\libraries
+Installation of libraries:
+- Extract zip archive "Bounce2.zip" to C:\Users\<YOURUSERNAME>\AppData\Local\Arduino15\libraries
+- Extract zip archive "DigisparkKeyboard-master-fixed.zip" to C:\Users\<YOURUSERNAME>\AppData\Local\Arduino15\libraries
 
-FIX/Change to original Master:
+FIX/Change to original DigisparkKeyboard-master.zip:
 - Headerfile "digi_de_de.h" is now working for proper ASCII output
