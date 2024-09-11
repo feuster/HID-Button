73d90c25e8c3f0aa03045f65b315d734724ded35
feuster <alexander.feuster@gmail.com>
2024-09-11 20:15:49 +0200

    Added support for sedond login
    - added support for a second login credential pair on a long button press
    - refactored setup() DigiKeyboard.delay() as KeepAlive to stabilize USB communication
    - LED blinks in setup() now for the whole initialize delay which signalizes better than the previous short blink at the end
    - refactored login into a  function to keep propgram size small
    - added another DigiKeyboard.delay() as KeepAlive to stabilize USB communication
    - lowered Bounce2 button interval to be more responsive
    - updated comments in sketch which were not informative enough
    - updated README

--- HID-Button.ino
+++ HID-Button.ino
@@ -1,84 +1,113 @@
 //--------------------------------------------//
-//              HID-Button V1.1               //
+//              HID-Button V1.2               //
 //         (c) Alexander Feuster 2024         //
 //  http://www.github.com/feuster/HID-Button  //
 //--------------------------------------------//
 
 // define german keyboard layout
 #define kbd_de_de
+// includes
 #include "DigiKeyboard.h"
 #include "Bounce2.h"
 
 /// --------------------------------------------------------------------------------------------
-/// login credentials which can be changed to real credentials
+/// login credentials pair 1 for short button press which can be changed to real credentials
 /// --------------------------------------------------------------------------------------------
-const char USER[] = "USER";
-const char PASSWORD[] = "PASSWORD";
+const char USER1[] = "USER1";
+const char PASSWORD1[] = "PASSWORD1";
+/// --------------------------------------------------------------------------------------------
+/// login credentials pair 2 for long button press which can be changed to real credentials
+/// --------------------------------------------------------------------------------------------
+const char USER2[] = "USER2";
+const char PASSWORD2[] = "PASSWORD2";
 // do not change code behind this line if not needed !!!
 /// --------------------------------------------------------------------------------------------
 
 /// --------------------------------------------------------------------------------------------
 /// hardware definitions
 /// --------------------------------------------------------------------------------------------
-// input pin for pushbutton
+// input pin for pushbutton (PB0-PB4 with according wiring change, never use PB5 which is RESET)
 const int buttonPin = PB0;
 // state of te builtin LED
 int previousLedState = HIGH;
+// time to define a long button press in ms
+int longPressTimeSpan = 1000;
 // create a Bounce2::Button object
 Bounce2::Button button = Bounce2::Button();
+// map an own virtual key for TAB which is missing in DigiKeyboard.h
+const int KEY_TAB = 43;
 
 /// --------------------------------------------------------------------------------------------
 /// start the initialization
 /// --------------------------------------------------------------------------------------------
 void setup() {
-  // configure the pushbutton pin as input:
+  // configure the pushbutton pin as input with internal pullup resistor
   pinMode(buttonPin, INPUT_PULLUP);
-
-  // wait some time before first run, to give hardware time to initialize
-  DigiKeyboard.delay(2500);
 
   // Bounce2::Button configuration
   button.attach(buttonPin, INPUT_PULLUP);
-  button.interval(100);
+  button.interval(5);
   button.setPressedState(LOW); 
 
-  // blink the builtin LED as start confirmation
-  for (int i = 0; i < 10; i++)
+  // wait some time before first run, to give hardware time to initialize and blink the builtin LED as start confirmation
+  for (int i = 0; i < 25; i++)
   {
     digitalWrite(LED_BUILTIN, previousLedState);
     previousLedState = !previousLedState;
-    delay(100);
+    DigiKeyboard.delay(100);
   }
   digitalWrite(LED_BUILTIN, LOW);
 }
 
+/// --------------------------------------------------------------------------------------------
 /// program loop
+/// --------------------------------------------------------------------------------------------
 void loop() {
-  // read the pushbutton state
+  // read the pushbutton state once per loop
   button.update();
 
-  // if the button state has changed, and it's currently pressed type login credentials
-  if (button.pressed()) 
+  // validate if button was pressed short
+  if (button.released() && button.previousDuration() < longPressTimeSpan) 
   {
-    // turn the builtin LED on while login is typed
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    // type out login credentials (remark: sendKeyStroke(0) is used as workaround for compatibility problems with older hardware and can be removed if this isn't needed)
-    DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.print(USER);
+    // type login credentials pair 1
+    Type(USER1, PASSWORD1);
+  }
+  // validate if button was pressed long
+  else if (button.released() && button.previousDuration() > longPressTimeSpan) 
+  {
+    // type login credentials pair 2
+    Type(USER2, PASSWORD2);
+  }
+  else
+  {
+    // keep alive internal update
     DigiKeyboard.delay(125);
-    DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.sendKeyStroke(43); //translates to TAB
-    DigiKeyboard.delay(125);
-    DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.print(PASSWORD);
-    DigiKeyboard.delay(125);
-    DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.sendKeyStroke(KEY_ENTER);
-
-    // turn the builtin LED off after login has been typed
-    digitalWrite(LED_BUILTIN, LOW);
   }
 
   ///program loop ends and will repeat now in a new cycle
+}
+
+/// --------------------------------------------------------------------------------------------
+/// type the desired user credentials as USB HID keyboard
+/// --------------------------------------------------------------------------------------------
+void Type(char User[], char Password[])
+{
+    // turn the builtin LED on while login is typed
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    // type login credentials pair (remark: sendKeyStroke(0) is used as workaround for compatibility problems with older hardware and can be removed if this isn't needed)
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.print(User);
+    DigiKeyboard.delay(125);
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.sendKeyStroke(KEY_TAB);  // switches from the user input field to the password input field (should work for 99% of login masks)
+    DigiKeyboard.delay(125);
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.print(Password);
+    DigiKeyboard.delay(125);
+    DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.sendKeyStroke(KEY_ENTER); // confirms login input
+
+    // turn the builtin LED off after login has been typed
+    digitalWrite(LED_BUILTIN, LOW);
 }
--- README.md
+++ README.md
@@ -16,8 +16,12 @@ The HID-Button acts as an automatic keyboard replacement which types credentials
 - Extract zip archive "Bounce2.zip" to C:\Users\<YOURUSERNAME>\AppData\Local\Arduino15\libraries
 - Extract zip archive "DigisparkKeyboard-master-fixed.zip" to C:\Users\<YOURUSERNAME>\AppData\Local\Arduino15\libraries
 - Extract the content of _DigisparkKeyboard-master-fixed.zip_ to __C:\Users\<YOURUSERNAME>\AppData\Local\Arduino15\libraries__ before compiling
-- Before uploading the INO sketch to the board you must change in [HID-Button.ino](./HID-Button.ino) the login credentials USER|PASSWORD to the desired real credentials
-- optional: if a reset is needed for your board you can connect _P05_ with a pushbutton to _GND_
+- Before uploading the INO sketch to the board you must change in [HID-Button.ino](./HID-Button.ino) the login credentials USER|PASSWORD pairs to the desired real credentials
+- optional: if a reset is needed for your board you can connect _PB5_ with a pushbutton to _GND_
+
+## Usage
+- press the button shorter than 1 second to type credentials pair 1 as login
+- press the button longer than 1 second to type credentials pair 2 as login
 
 ## License / Copyright
 The HID-Button is licensed under [GPL-2.0-only](./LICENSE).
