7d615abe7d01306da556a9f8e7d4a4988f3759ec
feuster <alexander.feuster@gmail.com>
2024-09-12 18:34:51 +0200

    Refactoring
    - more DigiKeyboard.delay() calls for improving keep-alive as USB-HID device
    - text will now by typed char by char to with keep-alive delays to prevent freezing when typing longer texts
    - re-ordered setup() steps
    - clear potential Keyboard write errors for more stability

--- HID-Button.ino
+++ HID-Button.ino
@@ -1,5 +1,5 @@
 //--------------------------------------------//
-//              HID-Button V1.2               //
+//              HID-Button V1.3               //
 //         (c) Alexander Feuster 2024         //
 //  http://www.github.com/feuster/HID-Button  //
 //--------------------------------------------//
@@ -28,7 +28,7 @@ const char PASSWORD2[] = "PASSWORD2";
 /// --------------------------------------------------------------------------------------------
 // input pin for pushbutton (PB0-PB4 with according wiring change, never use PB5 which is RESET)
 const int buttonPin = PB0;
-// state of te builtin LED
+// state of the builtin LED for blinking
 int previousLedState = HIGH;
 // time to define a long button press in ms
 int longPressTimeSpan = 1000;
@@ -41,6 +41,14 @@ const int KEY_TAB = 43;
 /// start the initialization
 /// --------------------------------------------------------------------------------------------
 void setup() {
+  // wait some time before first run, to give hardware time to initialize and blink the builtin LED as start confirmation
+  for (int i = 0; i < 30; i++)
+  {
+    digitalWrite(LED_BUILTIN, previousLedState);
+    previousLedState = !previousLedState;
+    DigiKeyboard.delay(100);
+  }
+
   // configure the pushbutton pin as input with internal pullup resistor
   pinMode(buttonPin, INPUT_PULLUP);
 
@@ -49,14 +57,9 @@ void setup() {
   button.interval(5);
   button.setPressedState(LOW); 
 
-  // wait some time before first run, to give hardware time to initialize and blink the builtin LED as start confirmation
-  for (int i = 0; i < 25; i++)
-  {
-    digitalWrite(LED_BUILTIN, previousLedState);
-    previousLedState = !previousLedState;
-    DigiKeyboard.delay(100);
-  }
+  // defined LED switch off in case LED is still on after loop
   digitalWrite(LED_BUILTIN, LOW);
+  DigiKeyboard.delay(25);
 }
 
 /// --------------------------------------------------------------------------------------------
@@ -81,10 +84,10 @@ void loop() {
   else
   {
     // keep alive internal update
-    DigiKeyboard.delay(125);
+    DigiKeyboard.delay(25);
   }
 
-  ///program loop ends and will repeat now in a new cycle
+  ///program loop ends here and will repeat now in a new cycle
 }
 
 /// --------------------------------------------------------------------------------------------
@@ -94,20 +97,46 @@ void Type(char User[], char Password[])
 {
     // turn the builtin LED on while login is typed
     digitalWrite(LED_BUILTIN, HIGH);
-
+    DigiKeyboard.delay(50);
+    
     // type login credentials pair (remark: sendKeyStroke(0) is used as workaround for compatibility problems with older hardware and can be removed if this isn't needed)
     DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.print(User);
-    DigiKeyboard.delay(125);
+    DigiKeyboard.delay(50);
+    TypeInSingleChars(User);
+    DigiKeyboard.delay(50);
     DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.delay(50);
     DigiKeyboard.sendKeyStroke(KEY_TAB);  // switches from the user input field to the password input field (should work for 99% of login masks)
-    DigiKeyboard.delay(125);
+    DigiKeyboard.delay(50);
     DigiKeyboard.sendKeyStroke(0);
-    DigiKeyboard.print(Password);
-    DigiKeyboard.delay(125);
+    DigiKeyboard.delay(50);
+    TypeInSingleChars(Password);
+    DigiKeyboard.delay(50);
     DigiKeyboard.sendKeyStroke(0);
+    DigiKeyboard.delay(50);
     DigiKeyboard.sendKeyStroke(KEY_ENTER); // confirms login input
+    DigiKeyboard.delay(50);
 
+    // clear potential write error
+    if (DigiKeyboard.getWriteError() != 0)
+      DigiKeyboard.clearWriteError();
+    DigiKeyboard.delay(50);
+    
     // turn the builtin LED off after login has been typed
     digitalWrite(LED_BUILTIN, LOW);
+    DigiKeyboard.delay(50);
+}
+
+/// --------------------------------------------------------------------------------------------
+/// print a longer text as singles and keep alive Keyboard with special delay (this should prevent freezing when typing longer texts)
+/// --------------------------------------------------------------------------------------------
+void TypeInSingleChars(char Text[])
+{
+  // split text array in single chars and type char
+  for (byte i = 0; Text[i] != 0; i++)
+  {
+    DigiKeyboard.print(Text[i]);
+    // delay should prevent freezing when typing longer texts
+    DigiKeyboard.delay(10);
+  }
 }
